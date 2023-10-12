5fe0f991264f6cfbc51cacb7fb441b60977cc903
BrunoGatti <gatti.br15@gmail.com>
2023-10-12 11:27:15 +0200

    aggiunto consegna di calcolatrice

--- calcolatrice.py
+++ calcolatrice.py
@@ -1,3 +1,6 @@
+#scrivere un programma che prende in input una operazione (come ad esempio 13+3 oppure 29*12) e la svolge, il programma dovrebbe capire se l'input é nel formato giusto
+#e capire di che operazione si tratta (per semplicitá sviluppa solo + e * non serve farle tutte e quattro). Se la stringa non é nel formato corretto dovrebbe stampare un messaggio di errore 
+
 x=input('inserire testo operazione ')
 
 		
