89e48b3f2d3c03d08982ef2e0a0e5e29e64bced5
BrunoGatti <gatti.br15@gmail.com>
2023-10-12 12:58:27 +0200

    aggiunto calcolatrice

--- calcolatrice.py
+++ calcolatrice.py
@@ -3,5 +3,33 @@
 
 x=input('inserire testo operazione ')
 
-		
-			
+def parse_and_calc(stringa) :
+	i=0
+	primo_numero=""
+	secondo_numero=""
+	operatore=""
+	#tiro fuori il primo numero
+	while i<len(stringa) and stringa[i] in '0123456789':
+		primo_numero += stringa[i]
+		i+=1
+	print(primo_numero)
+	#trovo un carattere che non é un numero
+	if i>len(stringa) or stringa[i] not in '+*':return None
+
+	elif stringa[i] in '+*': #se il carattere é un operatore
+		operatore+=stringa[i]
+		i+=1
+	print(operatore)
+	while i<len(stringa) and stringa[i] in '0123456789':# tiro fuori il secondo numero
+		secondo_numero += stringa[i]
+		i+=1 
+	print(secondo_numero, "=")
+	if operatore=="+": return int(primo_numero)+int(secondo_numero)
+	elif (operatore=="*"): return int(primo_numero)*int(secondo_numero)
+
+risultato= parse_and_calc(x)
+
+if type(risultato)==int:
+	print(risultato)
+
+else: print("errore nell'input")	
